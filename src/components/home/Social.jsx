6fc5a20e01d04d53aa29ca6ea4e8fb505d722ce0
Micro Fortwonatus <microfortwonatus6@gmail.com>
2022-11-27 15:06:41 +0700

    about-responsive

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -6,8 +6,8 @@ const Social = () => {
       <a href="https://www.instagram.com/microfortwonatus" className="home__social-icon" target="_blank" rel="noreferrer">
         <i className="uil uil-instagram"></i>
       </a>
-      <a href="https://www.instagram.com/microfortwonatus" className="home__social-icon" target="_blank" rel="noreferrer">
-        <i className="uil uil-dribbble"></i>
+      <a href="https://www.linkedin.com/in/micro-fortwonatus-2b0160237/" className="home__social-icon" target="_blank" rel="noreferrer">
+      <i class="uil uil-linkedin-alt"></i>
       </a>
       <a href="https://github.com/microfortwonatus96" className="home__social-icon" target="_blank" rel="noreferrer">
         <i className="uil uil-github-alt"></i>
